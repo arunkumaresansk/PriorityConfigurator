293d57bfa58440bbf5205529950ab8be77b0a034
Arun Kumaresan <arun.kumaresan@yahoo.com>
2017-10-15 12:01:44 -0700

    Added some stub calls

--- src/main/java/com/prioritizer/controller/PrioritizerController.java
+++ src/main/java/com/prioritizer/controller/PrioritizerController.java
@@ -27,7 +27,7 @@ public class PrioritizerController {
 	DBConnector dbConnector;
 
 	@RequestMapping(method = RequestMethod.POST, value = "/priorities")
-	public ResponseEntity<ActionResponse> publishCoupon(@RequestBody PriorityList priorities) {
+	public ResponseEntity<ActionResponse> postPriorities(@RequestBody PriorityList priorities) {
 		ActionResponse response = new ActionResponse();
 		List<Feedback> feedbacks = new ArrayList<Feedback>();
 		boolean isOperationSuccess = false;
@@ -42,9 +42,23 @@ public class PrioritizerController {
 				isOperationSuccess ? HttpStatus.OK : HttpStatus.UNPROCESSABLE_ENTITY);
 		return responseEntity;
 	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/methods")
+	public ResponseEntity<String> getMethods() {
+		String priorities = "{\"priorities\":[{\"methods\":[{\"name\":\"testMethod1\",\"priority\":\"0\"},{\"name\":\"testMethod2\",\"priority\":\"3\"},{\"name\":\"testMethod3\",\"priority\":\"2\"},{\"name\":\"testMethod4\",\"priority\":\"2\"},{\"name\":\"testMethod5\",\"priority\":\"3\"},{\"name\":\"testMethod6\",\"priority\":\"4\"},{\"name\":\"testMethod7\",\"priority\":\"5\"},{\"name\":\"testMethod8\",\"priority\":\"4\"},{\"name\":\"testMethod9\",\"priority\":\"4\"},{\"name\":\"testMethod10\",\"priority\":\"4\"}],\"class\":\"com.test.aws.migration.TestClass1\"},{\"methods\":[{\"name\":\"testMethod1\",\"priority\":\"1\"},{\"name\":\"testMethod2\",\"priority\":\"1\"},{\"name\":\"testMethod3\",\"priority\":\"1\"},{\"name\":\"testMethod4\",\"priority\":\"0\"},{\"name\":\"testMethod5\",\"priority\":\"0\"},{\"name\":\"testMethod6\",\"priority\":\"0\"},{\"name\":\"testMethod7\",\"priority\":\"0\"},{\"name\":\"testMethod8\",\"priority\":\"0\"}],\"class\":\"com.test.aws.migration.TestClass2\"}]}";
+		ResponseEntity<String> responseEntity = new ResponseEntity<String>(priorities, HttpStatus.OK);
+		return responseEntity;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/rules")
+	public ResponseEntity<String> getRules() {
+		String helloWorld = "{\"priorityThreshold\":2,\"passPercentage\":50,\"abortOnFailure\":false}";
+		ResponseEntity<String> responseEntity = new ResponseEntity<String>(helloWorld, HttpStatus.OK);
+		return responseEntity;
+	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/priorities")
-	public ResponseEntity<PriorityList> getTestPriorities() {
+	public ResponseEntity<PriorityList> getPriorities() {
 		PriorityList priorityList = new PriorityList();
 		priorityList.setPriorities(new ArrayList<ClassMethods>());
 		ResponseEntity<PriorityList> responseEntity = new ResponseEntity<PriorityList>(priorityList,
@@ -53,7 +67,7 @@ public class PrioritizerController {
 	}
 
 	@RequestMapping(method = RequestMethod.DELETE, value = "/priorities/{identifier}")
-	public ResponseEntity<ActionResponse> CompleteOrder(@PathVariable String identifier) {
+	public ResponseEntity<ActionResponse> removePriorities(@PathVariable String identifier) {
 		PriorityIdentifier pIdentifier = new PriorityIdentifier(identifier);
 		ActionResponse response = new ActionResponse();
 		List<Feedback> feedbacks = new ArrayList<Feedback>();
